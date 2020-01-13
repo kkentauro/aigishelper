eba9f03b7304093d39dd4b658eee0a2c15e8f840
kkentauro <kkentauroaigis@gmail.com>
2020-01-13 08:15:33 -0500

    tl file format change
    change format of translation files to json

--- aigishelper/js/translate.js
+++ aigishelper/js/translate.js
@@ -12,39 +12,23 @@ aigishelper.make_patch_context = async function(patch_zip, filename) {
 	};
 	
 	// make inheritable context for folders
-	if(filename + "/" in patch_zip.files) {
+	if(patch_zip.folder(new RegExp(filename + "/$")).length > 0) {
 		context.root = patch_zip.folder(filename);
 		return context;
 	}
 	
-	if(!(filename in patch_zip.files)) {
+	if(!patch_zip.file(filename)) {
 		return context;
 	}
 	
-	// load b64 encoded text and decode
+	// load json-encoded text and decode
 	let patch_file = patch_zip.file(filename);
-	let text = await patch_file.async("text")
-		.then(b64str => atob(b64str));
-	
-	// split translation file into lines delimited
-	// line format: jp\0en\0\0\n
-	let regex = /(?<jp>[^\0]+)\0(?<en>[^\0]*)\0\0(?:\n|$)/g;
-	let matches = text.matchAll(regex);
-	let pairs = {};
-	let match = matches.next();
-	
-	// split lines into jp/en pairs
-	while(!match.done) {
-		let jp = aigishelper.ascii_to_utf8(match.value.groups["jp"]);
-		let en = aigishelper.ascii_to_utf8(match.value.groups["en"]);
-		pairs[jp] = pairs[jp] || en;
-		match = matches.next();
-	}
-	
-	
+	let jsonobj = await patch_file.async("text")
+		.then(jsonstr => JSON.parse(jsonstr));
+		
 	context.translate_string = function(str) {
 		assert(typeof(str) === "string", "can only patch strings");
-		return pairs[str] || str || "";
+		return jsonobj.pairs[str] || str || "";
 	}
 	
 	return context;
